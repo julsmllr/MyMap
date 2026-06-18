10ce745a88216455c05e3766f55d170c7e1eb558
Jules <julesmuller06@gmail.com>
2026-06-18 16:51:32 +0200

    Scan des ports actifs

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from scapy.layers.l2 import Ether, ARP, srp
+from scapy.layers.inet import IP, TCP, sr
 import psutil, ipaddress
 
 
 def getNetwork():
     dico = psutil.net_if_addrs()
-    
-    
+
     ip = dico["Wi-Fi"][1][1]
     mask = dico["Wi-Fi"][1][2]
+
     network = ipaddress.IPv4Network(f'{ip}/{mask}', strict=False)
     return str(network)
     
@@ -46,8 +47,18 @@ def main():
     print("-" * 40) 
     for i, j in dicoReponse.items():
         print(f"{i} - {j}")
-   
-    
-    
+        
+    # Scan des ports : 
+    for k, v in dicoReponse.items():
+        dicoReponse[k] = {"MAC" : v, "PORTS" : []}        
+        
+    for i in dicoReponse.keys():
+        lst_packet_TCP = [IP(dst = i)/TCP(dport = j, flags="S") for j in range(1, 1025)] #Pour avoir des objets différents et pas un même pointeur sur chaque packet sur chaque port
+        rep, non_rep = sr(lst_packet_TCP, timeout=5, verbose=0)
+        for j in range(len(rep)):
+            if rep[j][1]["TCP"].flags == "SA":
+                dicoReponse[i]["PORTS"].append(rep[j][0][TCP].dport)
+        print(f"IP : {i} - PORTS : {dicoReponse[i]["PORTS"]} ")
+        
 if __name__ == "__main__":
     main()
